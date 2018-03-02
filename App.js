f48f701737cc2eff1ffd703d526b4c261ca94cf4
rahul <rahulhaque07@gmail.com>
2018-03-02 21:50:38 +0600

    Library list component created

--- App.js
+++ App.js
@@ -1,23 +1,31 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './src/reducers';
+import { Header } from './src/modules/components/common';
+import LibraryList from './src/modules/techLibrary/components/LibraryList';
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <Provider store={createStore(reducers)}>
+                <View style={styles.container}>
+                    <Header headerText="Tech Stack" />
+                    <LibraryList />
+
+                    <Text>Open up App.js to start working on your app!</Text>
+                    <Text>Changes you make will automatically reload.</Text>
+                    <Text>Shake your phone to open the developer menu.</Text>
+                </View>
+            </Provider>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#e8e8e8',
+    },
 });
---
+++ src/modules/components/common/Spinner.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {View, ActivityIndicator} from 'react-native';
+
+const Spinner = (props) => {
+
+    const {size} = props;
+
+    return (
+        <View style={styles.spinnerStyle}>
+            <ActivityIndicator size={size || 'large'}/>
+        </View>
+    );
+};
+
+const styles = {
+    spinnerStyle: {
+        flex: 1,
+        alignSelf: 'center',
+        justifyContent: 'center',
+    }
+};
+
+export {Spinner};
